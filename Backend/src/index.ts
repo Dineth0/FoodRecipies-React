3f0ac440c1439aca4b4d233da75b8e045cb0b609
Dineth0 <dinethnakandala10@gmail.com>
2025-10-22 20:35:38 +0530

    add mongodb

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,4 +1,6 @@
+import { error } from "console"
 import express, { Application, Request, Response } from "express"
+import mongoose from "mongoose"
 
 
 
@@ -9,7 +11,12 @@ app.get("/",(req: Request, res: Response) =>{
     res.send("Hello TS Express")
 }) 
  
-
+const mongo = mongoose.connect("mongodb://localhost:27017/foodRecipies")
+mongo.then(() =>{
+    console.log("MongoDb Connected")
+}).catch((error) =>{
+    console.error(error)
+})
 
 app.listen(5000, () => {
     console.log("Server running")
