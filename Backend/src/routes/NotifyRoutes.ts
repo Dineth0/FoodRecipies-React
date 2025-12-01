26b8bb7067a5ee096357407f9b3457a31715ebc5
Dineth0 <dinethnakandala10@gmail.com>
2025-12-01 23:36:12 +0530

    add notification router

---
+++ Backend/src/routes/NotifyRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getNotifications, markAsRead } from "../controllers/notificationController";
+
+
+const router = Router()
+router.get("/", getNotifications)
+router.put("mark-read", markAsRead)
+
+export default router
