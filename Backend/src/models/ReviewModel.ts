3a5d23388a5aebcb1fe8fdeabc7f48a36464f4c6
Dineth0 <dinethnakandala10@gmail.com>
2025-12-05 10:40:09 +0530

    add review model

---
+++ Backend/src/models/ReviewModel.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+
+interface IReview extends Document{
+    _id: mongoose.Types.ObjectId
+    user: mongoose.Types.ObjectId
+    recipe: mongoose.Types.ObjectId
+    rating: number
+    description : string
+    date: Date
+}
+
+const reviewSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "UserId is required"]
+    },
+    recipe:{
+        type: Schema.Types.ObjectId,
+        ref: "Recipe",
+        required: [true, "RecipeId is required"]
+    },
+    rating:{
+        type: Number,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    date:{
+        type:Date,
+        required: true
+    }
+})
+export const Review = mongoose.model<IReview>("Review", reviewSchema)
