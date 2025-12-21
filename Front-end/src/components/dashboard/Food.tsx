e5f74a597218a38eb311dbc5451dfbfbbbbd2993
Dineth0 <dinethnakandala10@gmail.com>
2025-12-21 19:19:33 +0530

    update food table

--- Front-end/src/components/dashboard/Food.tsx
+++ Front-end/src/components/dashboard/Food.tsx
@@ -87,7 +87,8 @@ export  default function Foods(){
 
     return(
         <>
-        <div className="text-center text-gray-300 py-10">
+        <div className="bg-white/10 p-6 rounded-lg backdrop-blur-md">
+        <h2 className="text-2xl font-bold mb-4 text-white">Foods</h2>
             <div className='flex justify-end mb-4'>
                 <button className='flex items-center gap-1 text-green-400 hover:text-green-600 font-medium'
                 onClick={handleAddClick}
@@ -95,54 +96,56 @@ export  default function Foods(){
                     Add Food<IoMdAdd className='text-lg'/>
                 </button>
             </div>
-            <table className="w-full text-left text-sm">
-                <thead className="text-gray-300 border-b border-gray-700">
-                    <tr>
-                        <th className='py-2 px-4'>Food Name</th>
-                        <th className='py-2 px-4'>Catogery</th>
-                         <th className="py-2 px-4">Cuisine</th>
-                         <th className='py-2 px-4'>Description</th>
-                        <th className='py-2 px-4'>Images</th>
-                        <th className='py-2 px-4'>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {foods.map((food, index)=>(
-                        <tr key={index} className="border-b border-gray-800 hover:bg-white/5">
-                            <td className='py-2 px-4'>{food.name}</td>
-                            <td className='py-2 px-4'>{food.category}</td>
-                            <td className='py-2 px-4'>{food.cuisine}</td>
-                            <td className='py-2 px-4'>{food.description}</td>
-
-                            <td className="py-2 px-4 flex gap-2 justify-center">
-                                {food.images && food.images.length > 0 ? (
-                                    food.images.map((imgUrl, idx) => (
-                                    <img
-                                        key={idx}
-                                        src={imgUrl}
-                                        alt={`${food.name} ${idx + 1}`}
-                                        className="w-16 h-16 object-cover rounded-md"
-                                    />
-                                    ))
-                                ) : (
-                                    <span>No Image</span>
-                                )}
-                            </td>
-                            
-                            <td className='py-2 px-4'>
-                                <button className='text-blue-400 hover:text-blue-600 mx-2'
-                                 onClick={() =>handleEditFood(food)}>
-                                    <FaEdit/>
-                                </button>
-                                <button className='text-red-400 hover:text-red-600 mx-2'
-                                onClick={()=>handleDelete(food)}>
-                                    <FaTrash/>
-                                </button>
-                            </td>
+            <div className='w-full overflow-x-auto'>
+                <table className="w-full text-left text-sm text-gray-300 table-fixed min-w-[1000px]">
+                    <thead className="uppercase tracking-wider  bg-black/70 backdrop-blur-md border-b border-white/20">
+                        <tr>
+                            <th className='py-2 px-4 w-[15%]'>Food Name</th>
+                            <th className='py-2 px-4 w-[10%]'>Catogery</th>
+                            <th className="py-2 px-4 w-[10%]">Cuisine</th>
+                            <th className='py-2 px-4 w-[30%]'>Description</th>
+                            <th className='py-2 px-4 w-[25%] text-center '>Images</th>
+                            <th className='py-2 px-4 w-[10%] text-center'>Action</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {foods.map((food, index)=>(
+                            <tr key={index} className="border-b border-gray-800 hover:bg-black/25">
+                                <td className='py-2 px-4'>{food.name}</td>
+                                <td className='py-2 px-4'>{food.category}</td>
+                                <td className='py-2 px-4'>{food.cuisine}</td>
+                                <td className='py-2 px-4'>{food.description}</td>
+
+                                <td className="py-2 px-4 flex gap-2 justify-center">
+                                    {food.images && food.images.length > 0 ? (
+                                        food.images.map((imgUrl, idx) => (
+                                        <img
+                                            key={idx}
+                                            src={imgUrl}
+                                            alt={`${food.name} ${idx + 1}`}
+                                            className="w-16 h-16 object-cover rounded-md"
+                                        />
+                                        ))
+                                    ) : (
+                                        <span>No Image</span>
+                                    )}
+                                </td>
+                                
+                                <td className='py-2 px-4'>
+                                    <button className='text-blue-400 hover:text-blue-600 mx-2'
+                                    onClick={() =>handleEditFood(food)}>
+                                        <FaEdit/>
+                                    </button>
+                                    <button className='text-red-400 hover:text-red-600 mx-2'
+                                    onClick={()=>handleDelete(food)}>
+                                        <FaTrash/>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
 
             <div className="flex justify-center items-center gap-4 mt-10">
                 <button
